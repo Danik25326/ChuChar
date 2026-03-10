48c133051912eeb146856852e51a84e0fdcd9ab4
Данило <danikrut79@gmail.com>
2026-03-10 18:43:35 +0200

    Update chatRoutes.js

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createChat, getUserChats, getChatMessages, getChat } = require('../controllers/chatController');
+const { createChat, getUserChats, getChatMessages, getChat, getChatMembers } = require('../controllers/chatController');
 const { authenticateToken } = require('../middleware/auth');
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/', authenticateToken, createChat);
 router.get('/', authenticateToken, getUserChats);
 router.get('/:chatId', authenticateToken, getChat);
 router.get('/:chatId/messages', authenticateToken, getChatMessages);
+router.get('/:chatId/members', authenticateToken, getChatMembers);
 
 module.exports = router;
