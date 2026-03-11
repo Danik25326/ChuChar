51c623c37e72d7dd55a50a2696520fc18ac854e5
Данило <danikrut79@gmail.com>
2026-03-11 14:53:19 +0200

    Update Chat.jsx

--- web-client/src/pages/Chat.jsx
+++ web-client/src/pages/Chat.jsx
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 import axios from 'axios';
 import { AuthContext } from '../context/AuthContext';
 import { useTheme } from '../context/ThemeContext';
-import Avatar from '../components/Avatar';
+import Avatar from '../%20components/Avatar';
 import { AudioPlayer } from '../components/AudioPlayer';
 import { format } from 'date-fns';
 import { uk } from 'date-fns/locale';
