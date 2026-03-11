a156282dd88baaa2344e4d7a53019316ba52a772
Данило <danikrut79@gmail.com>
2026-03-11 13:51:37 +0200

    Update userRoutes.js

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getUser, searchUsers } = require('../controllers/userController');
+const { getUser, searchUsers, updateAvatar } = require('../controllers/userController');
 const { authenticateToken } = require('../middleware/auth');
+const { upload } = require('../controllers/uploadController');
 
 const router = express.Router();
 
 router.get('/search', authenticateToken, searchUsers);
 router.get('/:id', authenticateToken, getUser);
+router.post('/avatar', authenticateToken, upload.single('avatar'), updateAvatar);
 
 module.exports = router;
