10a6fb0c1fbf313055388ee6561d09c58b9bdee5
Данило <danikrut79@gmail.com>
2026-03-11 14:39:28 +0200

    Update AudioPlayer.jsx

--- web-client/src/ components/AudioPlayer.jsx
+++ web-client/src/ components/AudioPlayer.jsx
@@ -56,15 +56,7 @@ export default function AudioPlayer({ src }) {
   return (
     <div className="flex items-center space-x-2 w-full">
       <button onClick={togglePlay} className="text-neon-blue">
-        {isPlaying ? (
-          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-          </svg>
-        ) : (
-          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
-          </svg>
-        )}
+        {isPlaying ? '⏸️' : '▶️'}
       </button>
       <div ref={waveformRef} className="flex-1" />
       <span className="text-xs text-neon-text-secondary">
